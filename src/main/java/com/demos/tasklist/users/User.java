50265e647b8bdc915bddd354ec08b0d320d63845
Javier <jastincer1998@gmail.com>
2022-11-18 14:50:33 -0500

    Add User entity

---
+++ src/main/java/com/demos/tasklist/users/User.java
@@ -0,0 +1,17 @@
+package com.demos.tasklist.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  @Id private String id;
+  private String email;
+  private String password;
+}
