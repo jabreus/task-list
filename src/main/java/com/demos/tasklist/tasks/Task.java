676ed9f5747214c843861a7589c525a1a137974d
Javier <jastincer1998@gmail.com>
2022-11-18 14:51:53 -0500

    Update Task entity to include user ownership

--- src/main/java/com/demos/tasklist/tasks/Task.java
+++ src/main/java/com/demos/tasklist/tasks/Task.java
@@ -1,11 +1,21 @@
 package com.demos.tasklist.tasks;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@CompoundIndex(def = "{ id: 1, userId: 1 }")
 public class Task {
 
   @Id private String id;
   private String content;
+  @DBRef private String userId;
 }
