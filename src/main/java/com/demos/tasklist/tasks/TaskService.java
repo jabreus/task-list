6b823fee60094a71a977b2cc993062541e7e0d0a
Javier <jastincer1998@gmail.com>
2022-11-18 14:53:54 -0500

    Add task service

---
+++ src/main/java/com/demos/tasklist/tasks/TaskService.java
@@ -0,0 +1,43 @@
+package com.demos.tasklist.tasks;
+
+import com.demos.tasklist.tasks.exceptions.TaskNotFoundException;
+import com.demos.tasklist.users.User;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+  private final TaskRepository taskRepository;
+
+  private final ModelMapper modelMapper;
+
+  public Page<Task> getAllByUsers(@NonNull String userId, @NonNull PageRequest pageable) {
+    return taskRepository.findAllByUserId(userId, pageable);
+  }
+
+  public Task create(CreateTaskRequest createTaskRequest, User user) {
+    var task = modelMapper.map(createTaskRequest, Task.class);
+    task.setUserId(user.getId());
+    return taskRepository.save(task);
+  }
+
+  public Task update(
+      @NonNull String taskId, @NonNull UpdateTaskRequest updateTaskRequest, @NonNull User user)
+      throws TaskNotFoundException {
+    var task = getById(taskId, user);
+    task.setContent(updateTaskRequest.getContent());
+    return taskRepository.save(task);
+  }
+
+  public Task getById(String taskId, User user) throws TaskNotFoundException {
+    return taskRepository
+        .findByIdAndUserId(taskId, user.getId())
+        .orElseThrow(() -> new TaskNotFoundException(taskId));
+  }
+}
