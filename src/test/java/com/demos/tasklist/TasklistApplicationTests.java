4c5367dd147dc911315355b1f7fde892688af8b4
Javier <jastincer1998@gmail.com>
2022-11-18 14:04:12 -0500

    First commit

---
+++ src/test/java/com/demos/tasklist/TasklistApplicationTests.java
@@ -0,0 +1,11 @@
+package com.demos.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
