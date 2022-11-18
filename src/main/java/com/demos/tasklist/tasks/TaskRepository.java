6f122ff7e6918210ae85fcfa40ce0df40c485f3a
Javier <jastincer1998@gmail.com>
2022-11-18 14:52:13 -0500

    Update TaskRepository to include custom methods

--- src/main/java/com/demos/tasklist/tasks/TaskRepository.java
+++ src/main/java/com/demos/tasklist/tasks/TaskRepository.java
@@ -1,7 +1,19 @@
 package com.demos.tasklist.tasks;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface TaskRepository extends MongoRepository<String, Task> {}
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+  @Query("{ userId: ?0 }")
+  Page<Task> findAllByUserId(String userId, PageRequest pageable);
+
+  @Query("{ taskId: ?0, userId: ?1 }")
+  Optional<Task> findByIdAndUserId(String taskId, String getId);
+}
