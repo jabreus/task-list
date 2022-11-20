eedff90bcc172186ebc3f8c30a71b8d892394351
Javier <jastincer1998@gmail.com>
2022-11-20 00:42:09 -0500

    Simplify UpdateTaskRequest annotations

--- src/main/java/com/demos/tasklist/tasks/dtos/UpdateTaskRequest.java
+++ src/main/java/com/demos/tasklist/tasks/dtos/UpdateTaskRequest.java
@@ -1,13 +1,13 @@
 package com.demos.tasklist.tasks.dtos;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
-@Data
-@RequiredArgsConstructor
+@Getter
+@Setter
 public class UpdateTaskRequest {
   @NotBlank(message = "[content] property is required")
-  private final String content;
+  private String content;
 }
