a48d1d48693c2e876ef6efddd3703dced74a632b
Javier <jastincer1998@gmail.com>
2022-11-20 00:41:51 -0500

    Simplify TaskDto annotations

--- src/main/java/com/demos/tasklist/tasks/dtos/TaskDto.java
+++ src/main/java/com/demos/tasklist/tasks/dtos/TaskDto.java
@@ -1,15 +1,12 @@
 package com.demos.tasklist.tasks.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class TaskDto {
+  private String id;
   private String content;
   private String userId;
 }
